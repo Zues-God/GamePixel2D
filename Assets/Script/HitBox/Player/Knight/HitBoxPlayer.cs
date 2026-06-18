ae42fd6047768dcc98470eabeec225b09bd1bc93
Zues-God <nbahung526@gmail.com>
2026-06-18 17:10:38 +0700

    Fix big dame player knight and update enemy map sand 1-3

--- Assets/Script/HitBox/Player/Knight/HitBoxPlayer.cs
+++ Assets/Script/HitBox/Player/Knight/HitBoxPlayer.cs
@@ -4,24 +4,17 @@ public class HitBox : MonoBehaviour
 {
 
     public float damage = 10f;
-    private void OnTriggerStay2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
-        {   
-          Player player = other.GetComponent<Player>();
-            if (player != null)
-            {
-                player.TakeDamage(damage);
-            }
-
-            
-        } else if (other.CompareTag("Enemy"))
+       if (other.CompareTag("Enemy"))
         {
             Enemy enemy = other.GetComponent<Enemy>();
             if (enemy != null)
             {
                 enemy.TakeDamage(damage, transform.root);
+                Debug.Log("Hit enemy " + Time.frameCount);
                 Debug.Log(damage);
+               
                 
             }
         }
