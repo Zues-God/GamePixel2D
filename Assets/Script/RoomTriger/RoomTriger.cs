65ed0b9d56711e231db3cbc279c146471736bf4e
PhamNguyenKhanh <pnk1590@gmail.com>
2026-08-01 02:22:40 +0700

    Revert "update"
    This reverts commit e6031ec989c2b065c285e260d85f303f7120f9ff.

--- Assets/Script/RoomTriger/RoomTriger.cs
+++ Assets/Script/RoomTriger/RoomTriger.cs
@@ -37,7 +37,8 @@ public class RoomTriger : MonoBehaviour
 
             if (isBossRoom)
             {
-
+                if (bossHPBar != null)
+                    bossHPBar.SetActive(true);
 
                 if (bossSound != null)
                     bossSound.Play();
@@ -149,8 +150,6 @@ public class RoomTriger : MonoBehaviour
         if (introBoss != null) introBoss.SetActive(false);
 
         Time.timeScale = 1f;
-        if (bossHPBar != null)
-            bossHPBar.SetActive(true);
 
         GetComponent<Collider2D>().enabled = false;
     }
